186e2e80222be3559ef5609f9f92615695c4e0fc
DHUer <961759850@qq.com>
2019-05-05 01:09:26 +0800

    查词弹窗基本完成, TODO: 生词本

--- 查词和取词/index.js
+++ 查词和取词/index.js
@@ -1,66 +1,81 @@
 //index.js
 //获取应用实例
 const app = getApp()
-const popUp = require('wordPop.js')
+const sha = require('sha256.js')
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    showModal: false,
+    result: null,
+    usPhonetic: null,
+    ukPhonetic: null,
     txtArray:[{
       id:1,
-      text:'as a result'
+      text:'dsss ddd'
     },{
       id:2,
-      text:'Word',
+      text:'as a result',
+      }, {
+        id: 2,
+        text: 'results',
     }],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onReady(e){
+    this.audioCtx = wx.createAudioContext('myAudio')
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  audioPlay() {
+    this.audioCtx.play()
+  },
+
+  //查询单词, 这里巧遇js中浅拷贝和深拷贝问题, 解决方案是:JSON.parse(JSON.stringify(source))
+  popUp: function lookUp(event) {
+    var that = this;
+    var words = event.currentTarget.dataset.text;
+    function getInput(input) {
+      if (input.length == 0) {
+        return null;
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
-      
-    })
-  },
-  popUp:function(event){ 
-    popUp.lookUp(event);
-    
+      var result;
+      var len = input.length;
+      if (len <= 20) {
+        result = input;
+      } else {
+        var startStr = input.substring(0, 10);
+        var endStr = input.substring(len - 10, len);
+        result = startStr + len + endStr;
+      }
+      return result;
+  
+    };
+  
+    var appKey = "4b4fc6a27d8efcd1";
+    var key = "HAoKg2mH8PlbrOKOWkZMKxKthOxmHAky";
+    var salt = new Date().getTime();
+    var curtime = Math.round(new Date().getTime() / 1000);
+    var from = 'auto';
+    var to = 'zh-CHS';
+    var str1 = appKey + getInput(words) + salt + curtime + key;
+    var sign = sha.SHA256(str1);
+    wx.request({
+      url: 'https://openapi.youdao.com/api',
+      data:{
+        q: words,
+        appKey: appKey,
+        salt: salt,
+        from: from,
+        to: to,
+        curtime: curtime,
+        sign: sign,
+        signType: "v3"
+      },
+      success: function (response) {
+        that.setData({
+          showModal: true,
+          result: response.data,
+          ukPhonetic: response.data.basic["uk-phonetic"],
+          usPhonetic: response.data.basic["us-phonetic"],
+        });
+        console.log(that.data.result);
+      }
+    });
   }
 })
