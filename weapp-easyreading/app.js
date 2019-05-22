5f99ec8b2657be288ebc08cc6a1573e0063e68ff
DHUer <961759850@qq.com>
2019-05-23 00:10:49 +0800

    登陆流程和后端代码梳理了一下，TODO：完成复习单词的前端逻辑

--- weapp-easyreading/app.js
+++ weapp-easyreading/app.js
@@ -5,7 +5,29 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
+    //这一步用来保存用户的openID和sessionID
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url:'192.168.1.103',
+          data:{
+            code: res.code
+          },
+          sucess: function(res){
+            //保存用户的信息到本地缓存之后每次都携带该openID和sessionID
+            setStorage({
+              openID: res.openID,
+              sessionID: res.sessionID
+            })
+          }
+        })
+      }
+    })
+    console.log(getStorage('openID'))
   },
+  
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
@@ -16,12 +38,14 @@ App({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
+              console.log(res)
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
           })
         }
-      })
+      }
+      )
     }
   },
   globalData:{
